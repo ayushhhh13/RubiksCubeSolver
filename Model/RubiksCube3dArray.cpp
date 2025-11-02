000332d7e969cc6802294401633f85a1724bdd3a
ayushhhh13 <authenticayush13@gmail.com>
2025-11-02 16:32:10 +0530

    Starting with 3D Representation

---
+++ Model/RubiksCube3dArray.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Ayush Agrawal on 02/11/25.
+//
+
+#include "GenericRubiksCube.h"
+
+class RubiksCube3dArray : public GenericRubiksCube {
+private:
+
+    void rotateFace(int ind) {
+        char temp_arr[3][3] = {};
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                temp_arr[i][j] = cube[ind][i][j];
+            }
+        }
+        for (int i = 0; i < 3; i++) cube[ind][0][i] = temp_arr[2 - i][0];
+        for (int i = 0; i < 3; i++) cube[ind][i][2] = temp_arr[0][i];
+        for (int i = 0; i < 3; i++) cube[ind][2][2 - i] = temp_arr[i][2];
+        for (int i = 0; i < 3; i++) cube[ind][2 - i][0] = temp_arr[2][2 - i];
+    }
+
+public:
+    char cube[6][3][3]{};
+
+    RubiksCube3dArray() {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++)
+                    cube[i][j][k] = getColorLetter(COLOR(i));
+            }
+        }
+    }
+};
