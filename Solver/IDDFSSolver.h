b989c7fbf0ab1ea3fc8f17794ddbb847489dca3f
ayushhhh13 <authenticayush13@gmail.com>
2025-11-23 19:40:35 +0530

    Implemented All 3 Solvers

---
+++ Solver/IDDFSSolver.h
@@ -0,0 +1,41 @@
+//
+// Created by Ayush Agrawal on 23/11/25.
+//
+
+#ifndef RUBIKSCUBESOLVER_IDDFSSOLVER_H
+#define RUBIKSCUBESOLVER_IDDFSSOLVER_H
+
+#include<bits/stdc++.h>
+#include "../Model/GenericRubiksCube.h"
+#include "DFSSolver.h"
+
+template<typename T, typename H>
+class IDDFSSolver {
+
+private:
+    int max_search_depth;
+    vector<GenericRubiksCube::MOVE> moves;
+
+public:
+    T rubiksCube;
+
+    IDDFSSolver(T _rubiksCube, int _max_search_depth = 7) {
+        rubiksCube = _rubiksCube;
+        max_search_depth = _max_search_depth;
+    }
+
+    //    Used DFSSolver with increasing max_search_depth
+    vector<GenericRubiksCube::MOVE> solve() {
+        for (int i = 1; i <= max_search_depth; i++) {
+            DFSSolver<T, H> dfsSolver(rubiksCube, i);
+            moves = dfsSolver.solve();
+            if (dfsSolver.rubiksCube.isSolved()) {
+                rubiksCube = dfsSolver.rubiksCube;
+                break;
+            }
+        }
+        return moves;
+    }
+};
+
+#endif //RUBIKSCUBESOLVER_IDDFSSOLVER_H
